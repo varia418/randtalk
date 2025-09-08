0a9363225883b8b90f4aa1414ccda9fcc7719eee
varia <ltnhan2610@gmail.com>
2025-09-08 16:08:25 +0700

    extract toolbar into a component

---
+++ src/components/ToolBar.tsx
@@ -0,0 +1,52 @@
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./ui/select";
+import { useState } from "react";
+
+function ToolBar() {
+	const [sorting, setSorting] = useState("most-recent");
+
+	return (
+		<div className="flex mt-10 gap-4">
+			<Button className="bg-blue-500 hover:bg-blue-600 text-white">
+				<Plus />
+				Create a Room
+			</Button>
+			<div className="flex gap-4 ml-auto">
+				<Select value={sorting} onValueChange={setSorting}>
+					<SelectTrigger className="w-[140px]">
+						<SelectValue />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value="most-recent">Most recent</SelectItem>
+						<SelectItem value="most-popular">
+							Most popular
+						</SelectItem>
+					</SelectContent>
+				</Select>
+				{/* <div className="flex w-full max-w-sm items-center space-x-2">
+						<Input
+							type="search"
+							placeholder="Search by title or id"
+							className="flex-1"
+						/>
+						<Button
+							type="submit"
+							size="icon"
+							className="bg-blue-500 hover:bg-blue-600"
+						>
+							<Search className="h-4 w-4" />
+						</Button>
+					</div> */}
+			</div>
+		</div>
+	);
+}
+
+export default ToolBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,8 @@
-import { Button } from "@/components/ui/button";
+import ToolBar from "@/components/ToolBar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { Plus } from "lucide-react";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
-import { useState } from "react";
 
 function Home() {
-	const [sorting, setSorting] = useState("most-recent");
 	return (
 		<div className="min-h-screen container mx-auto flex flex-col">
 			<Header />
@@ -22,41 +12,7 @@ function Home() {
 			<p className="text-center text-xl">
 				No Sign-Up. Just Start Talking.
 			</p>
-			<div className="flex mt-10 gap-4">
-				<Button className="bg-blue-500 hover:bg-blue-600 text-white">
-					<Plus />
-					Create a Room
-				</Button>
-				<div className="flex gap-4 ml-auto">
-					<Select value={sorting} onValueChange={setSorting}>
-						<SelectTrigger className="w-[140px]">
-							<SelectValue />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectItem value="most-recent">
-								Most recent
-							</SelectItem>
-							<SelectItem value="most-popular">
-								Most popular
-							</SelectItem>
-						</SelectContent>
-					</Select>
-					{/* <div className="flex w-full max-w-sm items-center space-x-2">
-						<Input
-							type="search"
-							placeholder="Search by title or id"
-							className="flex-1"
-						/>
-						<Button
-							type="submit"
-							size="icon"
-							className="bg-blue-500 hover:bg-blue-600"
-						>
-							<Search className="h-4 w-4" />
-						</Button>
-					</div> */}
-				</div>
-			</div>
+			<ToolBar />
 			<Footer />
 		</div>
 	);
