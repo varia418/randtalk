1c1c4539fb6fa34afd4cbd72af193ac017607903
varia <ltnhan2610@gmail.com>
2025-09-09 10:59:06 +0700

    adjust grid column size

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 function ChatRoom() {
 	const { roomId } = useParams();
 	return (
-		<div className="min-h-screen grid grid-rows-[50px_minmax(300px,1fr)_100px] grid-cols-[200px_minmax(500px,1fr)_500px]">
+		<div className="min-h-screen grid grid-rows-[50px_minmax(300px,1fr)_100px] grid-cols-[200px_minmax(400px,1fr)_400px]">
 			<div className="border px-2 flex items-center">
 				<h1 className="text-2xl truncate">User List</h1>
 			</div>
