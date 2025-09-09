d273150d8e3144a5dd7418b6df3d8850ad671f8a
varia <ltnhan2610@gmail.com>
2025-09-09 14:06:24 +0700

    add camera placeholder

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -5,6 +5,7 @@ import { useLoaderData } from "react-router";
 
 function ChatRoom() {
 	const { room, users, messages } = useLoaderData();
+	const cameras = ["Camera 1", "Camera 2", "Camera 3"];
 
 	return (
 		<div className="min-h-screen grid grid-rows-[50px_minmax(300px,1fr)_100px] grid-cols-[200px_minmax(400px,1fr)_400px]">
@@ -22,14 +23,25 @@ function ChatRoom() {
 			<div className="border row-span-2">
 				<UserList users={users} />
 			</div>
-			<div className="border auto-rows-max auto-cols-max">
+			<div className="border auto-rows-max auto-cols-max pb-2">
 				<ul className="flex flex-col justify-end overflow-y-auto h-full">
 					{messages.map((message: Message) => (
 						<ChatMessage key={message.id} {...message} />
 					))}
 				</ul>
 			</div>
-			<div className="border">camera list</div>
+			<div className="border">
+				<div className="grid grid-cols-2 justify-items-center">
+					{cameras.map((camera, index) => (
+						<div
+							key={index}
+							className="border m-2 p-2 size-40 flex items-center justify-center"
+						>
+							{camera}
+						</div>
+					))}
+				</div>
+			</div>
 			<div className="border">input</div>
 			<div className="border">controls</div>
 		</div>
