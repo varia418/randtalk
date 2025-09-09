1ad9cca150a8eb91c169a2848d6e7dbf45721a7a
varia <ltnhan2610@gmail.com>
2025-09-09 10:02:16 +0700

    create a layout for the chat room

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -3,7 +3,22 @@ import { useParams } from "react-router";
 function ChatRoom() {
 	const { roomId } = useParams();
 	return (
-		<h1 className="text-3xl font-bold underline">{`Room: ${roomId}`}</h1>
+		<div className="min-h-screen grid grid-rows-[50px_1fr_100px] grid-cols-[200px_1fr_500px]">
+			<div className="border">User List</div>
+			<div className="border">GOTY watchalong</div>
+			<div className="border">Cameras</div>
+			<div className="border row-span-2">
+				<ul>
+					<li>varia</li>
+					<li>ruben</li>
+					<li>noah</li>
+				</ul>
+			</div>
+			<div className="border auto-rows-max auto-cols-max">messages</div>
+			<div className="border">camera list</div>
+			<div className="border">input</div>
+			<div className="border">controls</div>
+		</div>
 	);
 }
 
