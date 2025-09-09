3f365f983a6d2e17e38b2642025d0f5e7d7d9fc6
varia <ltnhan2610@gmail.com>
2025-09-09 11:51:45 +0700

    reduce loader delay for room data retrieval

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
 	{
 		path: "/room/:roomId",
 		loader: async () => {
-			await new Promise((resolve) => setTimeout(resolve, 3000));
+			await new Promise((resolve) => setTimeout(resolve, 500));
 			return {
 				room: {
 					id: "1",
