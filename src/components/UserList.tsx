c6fde4cf5d9e46d5cd6a6720a37db1272d25d68f
varia <ltnhan2610@gmail.com>
2025-09-09 11:54:55 +0700

    add key prop to UserList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -5,7 +5,7 @@ function UserList({ users }: { users: User[] }) {
 	return (
 		<ul className="flex flex-col gap-2 p-2">
 			{users.map((user) => (
-				<li className="truncate flex gap-2">
+				<li key={user.id} className="truncate flex gap-2">
 					<UserIcon />
 					<span>{user.name}</span>
 				</li>
