c578a5569c830025ecdba9e2adc031be098b2d37
varia <ltnhan2610@gmail.com>
2025-09-10 11:57:31 +0700

    load user data from session storage

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -1,9 +1,13 @@
 import type { User } from "@/types";
 import UserContext from "./UserContext";
 import { useState } from "react";
+import { SESSION_KEYS } from "@/constants";
 
 function UserProvider({ children }: { children: React.ReactNode }) {
-	const [user, setUser] = useState<User | null>(null);
+	const [user, setUser] = useState<User | null>(() => {
+		const storedUser = sessionStorage.getItem(SESSION_KEYS.user);
+		return storedUser ? JSON.parse(storedUser) : null;
+	});
 	return (
 		<UserContext.Provider value={{ user: user, setUser: setUser }}>
 			{children}
